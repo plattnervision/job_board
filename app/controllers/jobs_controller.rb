66e16cb4981e07c55cf3e63a7ccc7dad5a9dbfb7
sunbather <mjplattner@gmail.com>
2016-04-19 20:01:53 -0400

    Adding all the things WWEEEE

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,41 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    Job.create(job_params)
+
+    redirect_to jobs_path
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.update(params[:id],job_params)
+    redirect_to jobs_path
+  end
+
+  def destroy
+    @job = Job.destroy(params[:id])
+    redirect_to jobs_path
+  end
+
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+
+end
