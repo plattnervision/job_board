4ce7194b60b85069af8163d86b78b9134c1fa506
sunbather <mjplattner@gmail.com>
2016-05-05 21:34:08 -0400

    Beef up the default route
    added a helper method to sort columns
    added santiation methods to make sure bad folks don't do bad thing i guess
    columns can be sorted by ascending or descending order (beans)

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,9 @@
 class JobsController < ApplicationController
 
+  helper_method :sort_column, :sort_direction
+
   def index
-    @jobs = Job.all
+    @jobs = Job.order(sort_column + " " + sort_direction)
   end
 
   def new
@@ -35,10 +37,18 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
   end
 
+
   private
 
   def job_params
     params.require(:job).permit(:title, :description)
   end
 
+  def sort_column
+    Job.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'jobs#view'
   resources :jobs
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -10,6 +12,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
       get 'all_jobs' => 'jobs#view'
+      get 'sorted_index' => 'jobs#sorted_index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
